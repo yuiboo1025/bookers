1616ca9d758c6459ac4c94749e9d1b5f5a0e13af
yuiboo1025 <122716372+yuiboo1025@users.noreply.github.com>
2023-02-07 10:24:35 +0000

    [Add]create,index,update,destroy機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,12 @@ class BooksController < ApplicationController
   def new
     @book=Book.new
   end
-  
+
   def create
     book=Book.new(book_params)
     book.save
-    #リダイアル先は確認する。showであってる？＃
-    redirect_to '/show'
+    #リダイアル先showであってる？＃
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -15,14 +15,28 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
